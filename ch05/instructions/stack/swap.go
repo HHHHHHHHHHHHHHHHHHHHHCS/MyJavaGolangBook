5678fb154c3270387195d02ba7c6bcf42a7881ec
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-04-10 20:17:02 +0800

    add

---
+++ ch05/instructions/stack/swap.go
@@ -0,0 +1,18 @@
+package stack
+
+import (
+	"MyJavaGolangBook/ch05/instructions/base"
+	"MyJavaGolangBook/ch05/rtda"
+)
+
+type SWAP struct {
+	base.NoOperandsInstruction
+}
+
+func (self *SWAP) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	slot1 := stack.PopSlot()
+	slot2 := stack.PopSlot()
+	stack.PushSlot(slot1)
+	stack.PushSlot(slot2)
+}
