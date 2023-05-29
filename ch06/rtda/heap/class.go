f729eb04c0f02e2377f1e5e00bd74349b043a73a
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-05-29 20:20:54 +0800

    add

--- ch06/rtda/heap/class.go
+++ ch06/rtda/heap/class.go
@@ -1,6 +1,8 @@
 package heap
 
-import "MyJavaGolangBook/ch06/classfile"
+import (
+	"MyJavaGolangBook/ch06/classfile"
+)
 
 type Class struct {
 	accessFlags        uint16
@@ -21,5 +23,42 @@ type Class struct {
 func newClass(cf *classfile.ClassFile) *Class {
 	class := &Class{}
 	class.accessFlags = cf.AccessFlags()
-	//TODO:
+	class.name = cf.ClassName()
+	class.superClassName = cf.SuperClassName()
+	class.interfaceNames = cf.InterfaceNames()
+	class.constantPool = newConstantPool(class, cf.ConstantPool())
+	class.fields = newFields(class, cf.Fields())
+	class.methods = newMethodSet(class, cf.Methods())
+	return class
+}
+
+func (self *Class) IsPublic() bool {
+	return 0 != self.accessFlags&ACC_PUBLIC
+}
+func (self *Class) IsFinal() bool {
+	return 0 != self.accessFlags&ACC_FINAL
+}
+func (self *Class) IsSuper() bool {
+	return 0 != self.accessFlags&ACC_SUPER
+}
+func (self *Class) IsInterface() bool {
+	return 0 != self.accessFlags&ACC_INTERFACE
+}
+func (self *Class) IsAbstract() bool {
+	return 0 != self.accessFlags&ACC_ABSTRACT
+}
+func (self *Class) IsSynthetic() bool {
+	return 0 != self.accessFlags&ACC_SYNTHETIC
+}
+func (self *Class) IsAnnotation() bool {
+	return 0 != self.accessFlags&ACC_ANNOTATION
+}
+func (self *Class) IsEnum() bool {
+	return 0 != self.accessFlags&ACC_ENUM
+}
+
+func (self *ClassMember) copyMemberInfo(memberInfo *classfile.MemberInfo) {
+	self.accessFlags = memberInfo.AccessFlags()
+	self.name = memberInfo.Name()
+	self.descriptor = memberInfo.Descriptor()
 }
---
+++ ch06/rtda/heap/field.go
@@ -0,0 +1,20 @@
+package heap
+
+import (
+	"MyJavaGolangBook/ch06/classfile"
+)
+
+type Field struct {
+	ClassMember
+}
+
+func newFields(class *Class, cfFields []*classfile.MemberInfo) []*Field {
+	fields := make([]*Field, len(cfFields))
+	for i, cfFields := range cfFields {
+		field := Field{}
+		field.class = class
+		field.copyMemberInfo(cfFields)
+		fields[i] = &field
+	}
+	return fields
+}
