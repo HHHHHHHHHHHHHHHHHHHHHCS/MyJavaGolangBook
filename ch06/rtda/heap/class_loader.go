fbb9e100f27f612378d5f66dfa2e98570204b9d0
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-06-05 20:47:00 +0800

    add

---
+++ ch06/rtda/heap/class_loader.go
@@ -0,0 +1,50 @@
+package heap
+
+import (
+	"MyJavaGolangBook/ch06/classpath"
+	"fmt"
+)
+
+type ClassLoader struct {
+	cp       *classpath.Classpath
+	classMap map[string]*Class
+}
+
+func NewClassLoader(cp *classpath.Classpath) *ClassLoader {
+	return &ClassLoader{
+		cp:       cp,
+		classMap: make(map[string]*Class),
+	}
+}
+
+func (self *ClassLoader) LoadClass(name string) *Class {
+	if class, ok := self.classMap[name]; ok {
+		return class
+	}
+	return self.loadNonArrayClass(name)
+}
+
+func (self *ClassLoader) loadNonArrayClass(name string) *Class {
+	data, entry := self.readClass(name)
+	class := self.defineClass(data)
+	link(class)
+	fmt.Printf("[Loaded %s from %s]\n", name, entry)
+	return class
+}
+
+func (self *ClassLoader) readerClass(name string) ([]byte, classpath.Entry) {
+	data, entry, err := self.cp.ReadClass(name)
+	if err != nil {
+		panic("java.lang.ClassNotFoundException: " + name)
+	}
+	return data, entry
+}
+
+func (self *ClassLoader) defineClass(data []byte) *Class {
+	class := parseClass(data)
+	class.loader = self
+	resolveSuperClass(class)
+	resolveInterfaces(class)
+	self.classMap[class.name] = class
+	return class
+}
