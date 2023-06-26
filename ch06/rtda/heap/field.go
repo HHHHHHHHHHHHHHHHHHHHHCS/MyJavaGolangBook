8c86478b4abc64669488c02759eedcded1b7eb0b
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-06-26 21:02:12 +0800

    add

--- ch06/rtda/heap/field.go
+++ ch06/rtda/heap/field.go
@@ -6,7 +6,8 @@ import (
 
 type Field struct {
 	ClassMember
-	slotId uint
+	constValueIndex uint
+	slotId          uint
 }
 
 func newFields(class *Class, cfFields []*classfile.MemberInfo) []*Field {
@@ -15,7 +16,34 @@ func newFields(class *Class, cfFields []*classfile.MemberInfo) []*Field {
 		field := Field{}
 		field.class = class
 		field.copyMemberInfo(cfFields)
+		field.copyAttributes(cfFields)
 		fields[i] = &field
 	}
 	return fields
+}
+
+func (self *Field) copyAttributes(cfField *classfile.MemberInfo) {
+	if valAttr := cfField.ConstantValueAttribute(); valAttr != nil {
+		self.constValueIndex = uint(valAttr.ConstantValueIndex())
+	}
+}
+
+func (self *Field) IsVolatile() bool {
+	return 0 != self.accessFlags&ACC_VOLATILE
+}
+func (self *Field) IsTransient() bool {
+	return 0 != self.accessFlags&ACC_TRANSIENT
+}
+func (self *Field) IsEnum() bool {
+	return 0 != self.accessFlags&ACC_ENUM
+}
+
+func (self *Field) ConstValueIndex() uint {
+	return self.constValueIndex
+}
+func (self *Field) SlotId() uint {
+	return self.slotId
+}
+func (self *Field) isLongOrDouble() bool {
+	return self.descriptor == "J" || self.descriptor == "D"
 }
