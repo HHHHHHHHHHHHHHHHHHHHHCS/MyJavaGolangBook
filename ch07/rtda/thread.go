a56a23cac0231591e4d309e1203e06805441dacb
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-08-01 21:01:02 +0800

    add

--- ch07/rtda/thread.go
+++ ch07/rtda/thread.go
@@ -38,6 +38,10 @@ func (self *Thread) PopFrame() *Frame {
 	return self.stack.pop()
 }
 
+func (self *Thread) TopFrame() *Frame {
+	return self.stack.top()
+}
+
 func (self *Thread) CurrentFrame() *Frame {
 	return self.stack.top()
 }
