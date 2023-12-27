f685543b362948c77cf1acfb65043e3044d97ddc
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-12-27 21:11:49 +0800

    add

--- ch09/rtda/heap/class.go
+++ ch09/rtda/heap/class.go
@@ -35,6 +35,10 @@ func newClass(cf *classfile.ClassFile) *Class {
 	return class
 }
 
+func (self *Class) IsPrimitive() bool {
+	_, ok := primitiveTypes[self.name]
+	return ok
+}
 func (self *Class) IsPublic() bool {
 	return 0 != self.accessFlags&ACC_PUBLIC
 }
