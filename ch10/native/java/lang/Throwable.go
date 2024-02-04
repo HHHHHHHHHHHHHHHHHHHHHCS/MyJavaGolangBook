b8a6fa8b8bac7caefe9edf2d4c913b6699597c2e
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2024-02-04 21:06:33 +0800

    add

--- ch10/native/java/lang/Throwable.go
+++ ch10/native/java/lang/Throwable.go
@@ -3,7 +3,15 @@ package lang
 import (
 	"MyJavaGolangBook/ch10/native"
 	"MyJavaGolangBook/ch10/rtda"
+	"MyJavaGolangBook/ch10/rtda/heap"
 )
+
+type StackTraceElement struct {
+	fileName   string
+	className  string
+	methodName string
+	lineNumber int
+}
 
 func init() {
 	native.Register("java/lang/Throwable", "fillInStackTrace",
@@ -11,5 +19,41 @@ func init() {
 }
 
 func fillInStackTrace(frame *rtda.Frame) {
+	this := frame.LocalVars().GetThis()
+	frame.OperandStack().PushRef(this)
 
+	stes := createStackTraceElements(this, frame.Thread())
+	this.SetExtra(stes)
+}
+
+func createStackTraceElements(tObj *heap.Object, thread *rtda.Thread) []*StackTraceElement {
+	// 栈顶两帧正在执行 fillInStackTrace fillInStackTrace
+	skip := distanceToObject(tObj.Class()) + 2
+	frames := thread.GetFrames()[skip:]
+	stes := make([]*StackTraceElement, len(frames))
+	for i, frame := range frames {
+		stes[i] = createStackTraceElement(frame)
+	}
+	return stes
+}
+
+// 跳过异常的构造函数的帧数
+func distanceToObject(class *heap.Class) int {
+	distance := 0
+	for c := class.SuperClass(); c != nil; c = c.SuperClass() {
+		distance++
+	}
+	return distance
+}
+
+// TODO:
+func createStackTraceElement(frame *rtda.Frame) *StackTraceElement {
+	method := frame.Method()
+	class := method.Class()
+	return &StackTraceElement{
+		fileName:   class.SourceFile(),
+		className:  class.JavaName(),
+		methodName: method.Name(),
+		lineNumber: method.GetLineNumber(frame.NextPC() - 1),
+	}
 }
